d80fbeecfb418109fc0f4a2f7f94d468250f4781
Caleb <unconfigured@null.spigotmc.org>
2021-02-15 20:40:04 +1100

    ehhh idk remove some shit add some shit

--- 5/main.cpp
+++ 5/main.cpp
@@ -1,25 +1,50 @@
 /*
     Lesson 5 - what is a pointer and why are they great
-
-    oh boy, pointers
-
-    A pointer is a variable that stores the memory address as its value
-    - w3schools.com
-
-    A pointer is basically a variable which "points" to another "variable"
-    or block of memory. 
-    
-    But why? In java we didnt have to deal with them? and your telling me 
-    theyre great?
-
-    Theyre great because they allow for very specific control over memory,
-    what variable your changing, weather your copying by reference or by 
-    value in a function and generally allow more control over your code.
-
-    so lets just run throw some example code
 */
+
+#include <cstdio>
+
+void modifyCheese(int cheese)
+{
+    cheese = 5;
+}
+
+void modifyCheese2(int* cheese)
+{
+    *cheese = 5;
+}
 
 int main(int argc, const char** argv)
 {
+    // make cheese, an int
+    int cheese = 17;
+
+    // make a useless pointer
+    int* pointer;
+
+    // now its really useless
+    pointer = nullptr;
+
+    // assign it
+    pointer = &cheese;
+
+    // print cheeese
+    printf("Cheese is %d\n", cheese);
+
+    // get the data from the pointer
+    printf("Cheese is %d\n", *pointer);
+
+    // modify cheese
+    modifyCheese(cheese);
+
+    // print cheeese
+    printf("Cheese is %d\n", cheese);
+
+    // modify cheese, the sequel
+    modifyCheese2(pointer);
+
+    // print cheeese
+    printf("Cheese is %d\n", *pointer);
+    
     return 0;
 }
